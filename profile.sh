a0977ab212af3c080a05cb3e3067ec161e73bba8
jvillegasl <jvillegasl@uni.pe>
2024-12-28 00:42:24 -0500

    feat: Add VSCode settings and profile script for development environment

---
+++ profile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source ~/.bashrc
+
+export-model() {
+	python model/main.py
+}
+
+latest-model() {
+	local model=`ls model/build/*.pt | sort -r | head -n 1`
+	echo $PWD/$model
+}
+
+build-cpp() {
+	cd cpp
+	mkdir -p build
+	cd build
+
+	cmake ..
+	cmake --build . --config Release
+
+	cd ../..
+}
+
+pred() {
+	local model_path
+
+	if test -n "$1"; then
+		model_path=$1
+	elif test ! -t 0; then
+		read line
+		model_path=$line
+	fi
+
+	./cpp/build/Release/simple-torchscript.exe $model_path
+}
