c15366d09b66345f3eafca0acd104510708ff798
jvillegasl <jvillegasl@uni.pe>
2024-12-29 11:43:51 -0500

    feat: Implement model execution in index.ts and update TypeScript target to ES2021

--- node/index.ts
+++ node/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+import { resolve, join } from "path";
+import { execSync, execFileSync, spawn } from "child_process";
+
+async function main() {
+    const parentDir = resolve(__dirname, "..");
+
+    const modelsPath = join(parentDir, "model/build").replaceAll("\\", "/");
+    const modelPath = await execSync(
+        `ls ${modelsPath}/*.pt | sort -r | head -n 1`
+    )
+        .toString()
+        .trim();
+
+    const exePath = join(parentDir, "cpp/build/Release/simple-torchscript.exe");
+    // const data = await execFileSync(exePath, [modelPath]).toString();
+    // console.log(data);
+
+    const process = spawn(exePath, [modelPath]);
+
+    process.stdout.on("data", (data) => {
+        console.log(data.toString());
+    });
+}
+
+main();
